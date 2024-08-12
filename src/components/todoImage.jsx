c55507cfecb1b0353669707ac89d62256ee2727e
Gladson Routh <grauth65@gmail.com>
2024-08-12 13:54:37 +0530

    To-Do image path changed

--- src/components/todoImage.jsx
+++ src/components/todoImage.jsx
@@ -3,7 +3,7 @@ function TodoImage() {
     return(
         <>
             <div className="bg-black text-white mt-20 h-50 w-screen flex justify-center">
-                <img id="todo-image" src="../public/todo2.png" alt="ToDo-picture" width="400px" height="200px"></img>
+                <img id="todo-image" src="/todo2.png" alt="ToDo-picture" width="400px" height="200px"></img>
             </div>
             
         </>
